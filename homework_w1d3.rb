88d4335020e6c5754be3048f9781898bcce27b93
Mark Blanford <markytorture@hotmail.com>
2017-11-22 18:56:11 +0000

    Section B Complete

--- homework_w1d3.rb
+++ homework_w1d3.rb
@@ -1,58 +1,64 @@
-# Homework
+# # Homework
+#
+# # A. Given the following data structure:
+#
+#
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+#
+#
+# # 1. Add `"Edinburgh Waverley"` to the end of the array
+#
+# stops.push("Edinburgh Waverley")
+# p stops
+#
+# # 2. Add `"Glasgow Queen St"` to the start of the array
+#
+# stops.unshift("Glasgow Queen St")
+# p stops
+#
+# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+#
+# stops.insert(4, "Polmont")
+# p stops
+#
+# # 4. Work out the index position of `"Linlithgow"`
+#
+# p stops.index("Linlithgow")
+#
+# # 5. Remove `"Livingston"` from the array using its name
+#
+# stops.delete("Livingston")
+# p stops
+#
+# # 6. Delete `"Cumbernauld"` from the array by index
+#
+# stops.delete_at(2)
+# p stops
+#
+# # 7. How many stops there are in the array?
+#
+# p stops.count()
+#
+# # 8. How many ways can we return `"Falkirk High"` from the array?
+#
+# p stops[2]
+# p stops.slice(2)
+#
+# # 9. Reverse the positions of the stops in the array
+#
+# p stops.reverse()
+#
+# # 10. Print out all the stops using a for loop
+#
+# for stop in stops
+#   p stop
+# end
 
-# A. Given the following data structure:
 
 
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 
-# 1. Add `"Edinburgh Waverley"` to the end of the array
 
-stops.push("Edinburgh Waverley")
-p stops
-
-# 2. Add `"Glasgow Queen St"` to the start of the array
-
-stops.unshift("Glasgow Queen St")
-p stops
-
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-
-stops.insert(4, "Polmont")
-p stops
-
-# 4. Work out the index position of `"Linlithgow"`
-
-p stops.index("Linlithgow")
-
-# 5. Remove `"Livingston"` from the array using its name
-
-stops.delete("Livingston")
-p stops
-
-# 6. Delete `"Cumbernauld"` from the array by index
-
-stops.delete_at(2)
-p stops
-
-# 7. How many stops there are in the array?
-
-p stops.count()
-
-# 8. How many ways can we return `"Falkirk High"` from the array?
-
-p stops[2]
-p stops.slice(2)
-
-# 9. Reverse the positions of the stops in the array
-
-p stops.reverse()
-
-# 10. Print out all the stops using a for loop
-
-for stop in stops
-  p stop
-end
 
 ## B. Given the following data structure:
 
@@ -89,14 +95,48 @@ end
   }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+p users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
+
+p users ["Erik"][:home_town]
+
 # 3. Get the array of Erik's favourite numbers
+
+p users["Erik"][:favourite_numbers]
+
 # 4. Get the type of Avril's pet Colin
+
+p users["Avril"][:pets]["colin"]
+
 # 5. Get the smallest of Erik's favourite numbers
+
+p users["Erik"][:favourite_numbers].sort().first
+
 # 6. Add the number `7` to Erik's favourite numbers
+
+users["Erik"][:favourite_numbers] << 7
+p users["Erik"][:favourite_numbers]
+
 # 7. Change Erik's hometown to Edinburgh
+
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
+
 # 8. Add a pet dog to Erik called "Fluffy"
+
+users["Erik"][:pets]["fluffy"] = :dog
+p users["Erik"][:pets]
+
 # 9. Add yourself to the users hash
+
+users["Mark"] = {
+    :twitter => "markywildtype",
+    :favourite_numbers => [42, 69],
+    :home_town => "St. Paul's Cray"
+  }
+p users
 
 # C. Given the following data structure:
 
